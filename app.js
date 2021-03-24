11e467484613ee163cf3a8a86e8d0af3b223b072
shweyin <shweyin@gmail.com>
2021-03-24 15:25:17 -0400

    serve files

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const path = require("path");
 
 const PORT = process.env.PORT || 3000;
+
+app.use("/assets", express.static(__dirname + "/assets"));
 
 app.get("", (req, res) => {
     res.sendFile(path.join(__dirname + "/index.html"));
